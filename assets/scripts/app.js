c24b11720018042e1d09221f5f8026bfe15d7454
Aurélien Devaux <37188045+aureldvx@users.noreply.github.com>
2022-06-19 12:48:41 +0200

    chore: install bootstrap-icons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,9 @@
 import '../styles/app.scss';
-import 'bootstrap';
+import '@popperjs/core';
+import * as bootstrap from 'bootstrap';
 import Alpine from 'alpinejs';
+
+[].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 window.Alpine = Alpine;
 Alpine.start();
